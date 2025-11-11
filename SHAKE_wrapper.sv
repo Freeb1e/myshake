952afb1c0f68c83a65e865a2a8a4a7121b26e738
Freeb1e <lyzxhfzy2022>
2025-11-11 20:58:22 +0800

    增加了和sha3之间的连接

--- SHAKE_wrapper.sv
+++ SHAKE_wrapper.sv
@@ -2,16 +2,17 @@ module SHAKE_wrapper(
     input        clk,
     input        rst_n,
     input [31:0] seed_buffer,
-    //input        seed_valid,
     input [7:0]  absorb_num,//一共需要吸收的块的数量
     input [5:0]  last_block_bytes,//最后一块数据的字节数
     input        init,
     input        mode, // 0 for SHAKE128, 1 for SHAKE256
+
     output [31:0] dout,
     output logic [31:0] addr_seed,
     output       ready,
     output       valid 
     );
+
     logic [2:0] state,next_state;
     logic nreset;
     logic [5:0] cnt_absorb_once;
@@ -23,6 +24,7 @@ module SHAKE_wrapper(
     logic [6:0] addr_keccak;
     logic [7:0] ABSORB_TOTAL_CNT;
     logic [5:0] ABSORB_ONCE_CNT;
+    logic [2:0] last_state;
     assign ABSORB_TOTAL_CNT = absorb_num;
     assign ABSORB_ONCE_CNT = (mode==1'b0)?6'd42:6'd34;
     // FSM ctrl
@@ -54,8 +56,10 @@ module SHAKE_wrapper(
     always_ff@(posedge clk or negedge rst_n)begin
         if(!rst_n)begin
             state <= 3'b0;
+            last_state <= 3'b0;
         end
         else begin
+            last_state <= state;
             state<=next_state;
         end
     end
@@ -73,19 +77,23 @@ module SHAKE_wrapper(
                 cnt_absorb_once <= 6'd0;
                 cnt_absorb_total <= 8'd0;
                 absorb_done <= 1'b0;
-                PADDING_FLAG1 <= 1'b0;
-            end else if(state==ABSORB)begin
+                if(absorb_num<2) PADDING_FLAG1 <= 1'b1;
+                else PADDING_FLAG1 <= 1'b0;
+
+            end else if(state==ABSORB && ready)begin
                 if(cnt_absorb_once < ABSORB_ONCE_CNT - 1) begin
                     cnt_absorb_once <= cnt_absorb_once + 6'd1;
-                    if(PADDING_FLAG1==1'b1 && cnt_absorb_once == {last_block_bytes[5:2],2'b00}) begin
+                    if(PADDING_FLAG1==1'b1 && cnt_absorb_once == {2'b00,last_block_bytes[5:2]}-1) begin
                         PADDING_FLAG2 <= 1'b1;
                     end
+                    init_keccak <= 1'b0;
                 end
                 else begin
                     cnt_absorb_once <= 6'd0;
-                    if(cnt_absorb_total < ABSORB_TOTAL_CNT - 1)begin
+                    init_keccak <= 1'b1;
+                    if(cnt_absorb_total < ABSORB_TOTAL_CNT )begin
                         cnt_absorb_total <= cnt_absorb_total + 8'd1;
-                        if(cnt_absorb_total == absorb_num - 1) begin
+                        if(cnt_absorb_total == absorb_num - 2) begin
                             PADDING_FLAG1 <= 1'b1;
                         end
                     end
@@ -105,7 +113,7 @@ module SHAKE_wrapper(
             addr_keccak <= 7'd0;
         end
         else begin
-            if(state!=ABSORB) begin
+            if(last_state!=ABSORB) begin
                 addr_seed <= 32'd0;
                 wr_keccak <= 1'b0;
                 addr_keccak <= 7'd0;
@@ -124,11 +132,11 @@ module SHAKE_wrapper(
             PADDING_FLAG3 <= 1'b0;
         end
         else begin
-            if(state==ABSORB) begin
+            if(state==ABSORB && ready) begin
                 if(!PADDING_FLAG1 && !PADDING_FLAG2) begin
                     din <= seed_buffer;
                 end
-                else if(PADDING_FLAG2) begin
+                else if(PADDING_FLAG2&&!PADDING_FLAG3) begin
                     case(last_block_bytes[1:0])
                         2'b00: din <= 32'h0000001F;
                         2'b11: din <={8'h1F,seed_buffer[23:0]};
@@ -169,7 +177,7 @@ module SHAKE_wrapper(
     end
 
 // interface with sha3
-logic init_keccak,next;
+logic init_keccak,next,init_keccak_trig;
         sha3 u_sha3(
              .clk    	(clk     ),
              .nreset 	(nreset  ),
@@ -181,4 +189,22 @@ logic init_keccak,next;
              .next   	(next    ),
              .ready  	(ready   )
          );
+    rise_trigger u_rise_trigger(
+        .clk(clk),
+        .in_sig(init_keccak),
+        .out_sig(init_keccak_trig)
+    );
+endmodule
+
+
+module rise_trigger(
+    input clk,
+    input in_sig,
+    output logic out_sig
+    );
+    logic in_sig_dly;
+    always_ff@(posedge clk) begin
+        in_sig_dly <= in_sig;
+        out_sig <= in_sig & ~in_sig_dly;
+    end
 endmodule
